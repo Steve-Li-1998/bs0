36e7068a7249073b6794bb640436f1a3c865c474
steve <steve-li-1998@outlook.com>
2021-04-26 18:43:10 +0200

    对于fak.c进行了一些修正

---
+++ Prog/bs0/bs0/fak.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+//global initialisierte Variablen
+int x = 3;
+bool x1 = true;
+
+//global uninitialisierte Variablen
+int y;
+double y1;
+
+int fak(int n){
+	if(n < 0){
+		return NULL;
+	}
+    if(n==0){
+        return 1;
+    }
+    else{
+        return fak(n-1) * n;
+    }
+}
+
+int main()
+{   
+    //lokale Variablen
+    int z = 3;
+	char z1;
+
+    printf("Die Adresse von global initialisierte Variablen x ist %p\n", &x);
+	printf("Die Adresse von global initialisierte Variablen x1 ist %p\n", &x1);
+    printf("Die Adresse von global uninitialisierte Variablen y ist %p\n", &y);
+	printf("Die Adresse von global uninitialisierte Variablen y1 ist %p\n", &y1);
+    printf("Die Adresse von global lokale Variablen z ist %p\n", &z);
+	printf("Die Adresse von global lokale Variablen z1 ist %p\n", &z1);
+
+    printf("Die fakultaet von 7 lautet: %d\n", fak(7));
+
+    return 0;
+}
+
+
+
